bfe831a39e6d5406eb82b30c48c5d1747deff3bc
NITIN SINGHAL <singhal95nitin@gmail.com>
2018-04-06 01:33:25 +0530

    button added

--- Pitchperfect/ViewController.swift
+++ Pitchperfect/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func recordButton(_ sender: Any) {
+        print("Hello Nitin")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
